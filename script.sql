eb459c8cac09cb39f58fdde635b6ff7625e434e6
Josthin Ayon <oswayon9@hotmail.com>
2023-04-13 10:25:47 -0500

    plantilla funcional

---
+++ script.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS usuario;
+create table usuario(
+id_usuario serial,
+nombre varchar(100),
+apellido varchar(100),
+correo varchar(100) unique,
+clave varchar(100),
+roles json,
+constraint pk_usuario primary key (id_usuario)
+);
+
+
+INSERT INTO usuario(
+nombre, apellido, correo, clave, roles)
+VALUES
+(
+'Josthin', 'Ayon', 'oswayon9@hotmail.com',
+'$2y$13$yE9EI8TQZ04C9HWWmcpWOuLQbm8l/zAHa2SKr.EpkyQLhengUBMuS',
+'["ROLE_ADMIN"]'
+);
