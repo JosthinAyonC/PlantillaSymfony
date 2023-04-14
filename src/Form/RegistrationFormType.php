e83da04112ac283516ce88c8034ed8bdcd4e24dc
Josthin Ayon <oswayon9@hotmail.com>
2023-04-14 11:41:28 -0500

    update:form registro listo

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,7 +5,9 @@ namespace App\Form;
 use App\Entity\Usuario;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,9 +19,37 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('correo')
+            ->add('correo', EmailType::class,[
+                'label'=>'Email',
+                'attr' =>[
+                    'class'=>'form-control',
+                    'id' => 'floatingInput',
+                    'placeholder' => 'Correo'
+                ]
+            ])
+            ->add('nombre', TextType::class,[
+                'label'=>'Nombre',
+                'attr' =>[
+                    'class'=>'form-control',
+                    'id' => 'floatingInput',
+                    'placeholder' => 'Nombre'
+                ]
+            ])
+            ->add('apellido', TextType::class,[
+                'label'=>'Apellido',
+                'attr' =>[
+                    'class'=>'form-control',
+                    'id' => 'floatingInput',
+                    'placeholder' => 'Apellido'
+                ]
+            ])
+
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
+                'attr'=> [
+                    'class'=>'form-check-input',
+                    'id' => 'exampleCheck1',
+                ],
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
@@ -30,7 +60,13 @@ class RegistrationFormType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => [
+                    'autocomplete' => 'new-password',
+                    'name' => 'passord',
+                    'class'=>'form-control',
+                    'id' => 'floatingPassord',
+                    'placeholder' => 'Pasword'
+                    ],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
