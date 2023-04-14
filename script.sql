a531c91fff4abbdde5c380f2217bde292c70bd04
JosthinAyon <josthinayon@gmail.com>
2023-04-13 22:59:52 -0500

    Update base

--- script.sql
+++ script.sql
@@ -6,15 +6,16 @@ apellido varchar(100),
 correo varchar(100) unique,
 clave varchar(100),
 roles json,
+estado varchar(1),
 constraint pk_usuario primary key (id_usuario)
 );
 
 
 INSERT INTO usuario(
-nombre, apellido, correo, clave, roles)
+nombre, apellido, correo, clave, roles, estado)
 VALUES
 (
 'Josthin', 'Ayon', 'oswayon9@hotmail.com',
 '$2y$13$yE9EI8TQZ04C9HWWmcpWOuLQbm8l/zAHa2SKr.EpkyQLhengUBMuS',
-'["ROLE_ADMIN"]'
+'["ROLE_ADMIN"]', 'A' 
 );
